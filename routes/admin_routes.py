6c2a7d27831e59622a19275c6cfb24bee685afdb
Gabriely Carrari <gabymcarrari@gmail.com>
2024-10-30 13:57:04 -0300

    ♻️ alterações aula 23-10

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -9,8 +9,10 @@ from dtos.inserir_produto_dto import InserirProdutoDto
 from dtos.problem_details_dto import ProblemDetailsDto
 from models.pedido_model import EstadoPedido
 from models.produto_model import Produto
+from repositories.item_pedido_repo import ItemPedidoRepo
 from repositories.pedido_repo import PedidoRepo
 from repositories.produto_repo import ProdutoRepo
+from repositories.usuario_repo import UsuarioRepo
 
 
 router = APIRouter(prefix="/admin")
@@ -32,7 +34,7 @@ async def inserir_produto(inputDto: InserirProdutoDto) -> Produto:
 async def excluir_produto(inputDto: IdProdutoDto):
     if ProdutoRepo.excluir(inputDto.id_produto): return None
     pd = ProblemDetailsDto("int", f"O produto com id <b>{inputDto.id_produto}</b> não foi encontrado.", "value_not_found", ["body", "id_produto"])
-    return JSONResponse(pd.to_dict(), status_code=404)
+    return JSONResponse(pd.to_dict(), staatus_code=404)
 
 @router.get("/obter_produto/{id_produto}")
 async def obter_produto(id_produto: int = Path(..., title="Id do Produto", ge=1)):
@@ -50,18 +52,20 @@ async def alterar_produto(inputDto: AlterarProdutoDto):
 
 @router.post("/alterar_pedido", status_code=204)
 async def alterar_pedido(inputDto: AlterarPedidoDto):
-    if PedidoRepo.alterar_estado(inputDto.id, inputDto.estado): return None
+    if PedidoRepo.alterar_estado(inputDto.id, inputDto.estado.value): 
+        return None
     pd = ProblemDetailsDto("int", f"O pedido com id <b>{inputDto.id}</b> não foi encontrado.", "value_not_found", ["body", "id"])
     return JSONResponse(pd.to_dict(), status_code=404)
 
 @router.post("/cancelar_pedido/{id_pedido}", status_code=204)
-async def cancelar_pedido(id_pedido: int):
-    if PedidoRepo.alterar_estado(id_pedido, EstadoPedido.CANCELADO): return None
+async def cancelar_pedido(id_pedido: int = Path(..., title="Id do Pedido", ge=1)):
+    if PedidoRepo.alterar_estado(id_pedido, EstadoPedido.CANCELADO.value): 
+        return None
     pd = ProblemDetailsDto("int", f"O pedido com id <b>{id_pedido}</b> não foi encontrado.", "value_not_found", ["body", "id"])
     return JSONResponse(pd.to_dict(), status_code=404)
 
 @router.post("/evoluir_pedido/{id_pedido}", status_code=204)
-async def evoluir_pedido(id_pedido: int):
+async def evoluir_pedido(id_pedido: int = Path(..., title="Id do Pedido", ge=1)):
     pedido = PedidoRepo.obter_por_id(id_pedido)
     if not pedido:
         pd = ProblemDetailsDto("int", f"O pedido com id <b>{id_pedido}</b> não foi encontrado.", "value_not_found", ["body", "id"])
@@ -70,23 +74,29 @@ async def evoluir_pedido(id_pedido: int):
     estados = [e.value for e in list(EstadoPedido) if e != EstadoPedido.CANCELADO]
     indice = estados.index(estado_atual)
     indice += 1
-    if indice < len(estados): 
+    if indice < len(estados):
         novo_estado = estados[indice]
-        if PedidoRepo.alterar_estado(id_pedido, novo_estado): return None
+        if PedidoRepo.alterar_estado(id_pedido, novo_estado): 
+            return None
     pd = ProblemDetailsDto("int", f"O pedido com id <b>{id_pedido}</b> não pode ter seu estado evoluído para <b>cancelado</b>.", "state_change_invalid", ["body", "id"])
     return JSONResponse(pd.to_dict(), status_code=404)
-
     
 
 @router.get("/obter_pedido/{id_pedido}")
 async def obter_pedido(id_pedido: int = Path(..., title="Id do Pedido", ge=1)):
+    # TODO: refatorar criando Dto com resultado específico
     pedido = PedidoRepo.obter_por_id(id_pedido)
-    if pedido: return pedido
+    if pedido: 
+        itens = ItemPedidoRepo.obter_por_pedido(pedido.id)
+        cliente = UsuarioRepo.obter_por_id(pedido.id_cliente)
+        pedido.itens = itens
+        pedido.cliente = cliente
+        return pedido
     pd = ProblemDetailsDto("int", f"O pedido com id <b>{id_pedido}</b> não foi encontrado.", "value_not_found", ["body", "id"])
     return JSONResponse(pd.to_dict(), status_code=404)
 
 @router.get("/obter_pedidos_por_estado/{estado}")
-async def obter_pedidos_por_estado(estado: EstadoPedido = Path(..., title="Estado do Pedido")):
+async def obter_pedidos_por_estado(estado: EstadoPedido = Path(..., title="Estado do Pedido")):    
     await asyncio.sleep(1)
     pedidos = PedidoRepo.obter_todos_por_estado(estado.value)
     return pedidos
